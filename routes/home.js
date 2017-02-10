e1cf354ea42b614f6ff8f1005355948c9f328d47
Xinxin Xue <intelcoll@gmail.com>
2017-02-09 23:26:15 -0800

    chronological tweeter form completed

--- routes/home.js
+++ routes/home.js
@@ -18,46 +18,17 @@ router.get('/', function (req, res) {
 router.get('/dash', function (req, res) {
   let cookies = req.cookies;
   let date_ran = new Date();
-  let date ="Posted on: "+ date_ran.toDateString();
-  // let tweets = cookies.tweet;
-  // let name = cookies.name.split(",");
-  // let date = cookies.dates.split(",");
-  //  if (!tweets) {
-  //    tweets = [];
-  //    res.cookie('tweets', tweets, {maxAge: 3000000000});
-  //    res.render('dashboard', {faker, trending});
-  //  } else {
-  //    tweets = tweets.split(',');
-  //    res.render('dashboard', {tweets, trending});
-     res.render('dashboard', {trending, faker, cookies, date});
+  let date = date_ran.toDateString();
+  res.render('dashboard', {trending, faker, cookies, date});
  });
 
- router.post('/dash', function(req, res, next){
+ router.post('/dash', function(req, res){
    let params = req.body;
    let cookies = req.cookies;
-   res.cookie( `${params.name}`, params.tweet, {maxAge : 20000000} );
+  
+
+   res.cookie( `${params.user}`, params.tweet, {maxAge : 2592000000} );
    res.redirect('/dash');
  });
-
-// router.post('/dash', function(req, res) {
-//   const params = req.body;
-//   const user = params.user;
-//   const tweets = params.tweet; //user input
-//   let tweets = cookies.tweet; //the actual tweets
-//   if (tweets.length > 255) {
-//       tweets = "max 255 characters";
-//     } else {
-//       if (!tweets) {
-//         tweets = tweets;
-//       } else {
-//         tweets = tweets.split(',');
-//       }
-//     }
-//   res.cookie('tweets', tweets, {maxAge:2592000000 });
-//   res.render('dashboard', {tweets, trending});
-//   res.redirect('/dash');
-// });
-
-
 
 module.exports = router;
